8391a80d8201e4ba2bc6a289c0d7c07a62ce6d95
ichiakevin <105984255+ichiakevin@users.noreply.github.com>
2022-05-21 23:54:55 +0800

    Add files via upload

---
+++ guessing_game29.py
@@ -0,0 +1,22 @@
+#猜數字遊戲
+
+secrt_num = 80
+guess = None
+guess_count = 0
+guess_limit = 3
+oout_of_linit = False 
+while secrt_num != guess and not(oout_of_linit): 
+    guess_count += 1
+    if guess_count <= guess_limit:   
+     guess = int(input("請輸入數字: "))
+     if guess > secrt_num:
+         print("小一點")
+     elif guess < secrt_num:
+         print("大一點")
+    else:
+        out_of_linit = True 
+
+if out_of_linit:
+    print("哈哈你超爛，輸了")
+else:                
+    print("可惡你贏了")
